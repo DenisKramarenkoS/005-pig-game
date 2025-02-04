e0272056079ae08b20e2be18450d1985d3faa80e
Denis Kramarenko <cashflow.dkm@gmail.com>
2025-02-04 20:03:34 +1000

    Create UI and small JS logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-"use strict"
+'use strict';
+
+// Variables
+let pointsRoll = 0; // points for one roll
+
+// DOM
+const btnRoll = document.getElementById('-btn__roll');
+const img = document.getElementById('dice');
+
+btnRoll.addEventListener('click', () => {
+  pointsRoll = Math.trunc(Math.random() * 6) + 1;
+  img.src = `assets/images/dice-${pointsRoll}.svg`;
+});
